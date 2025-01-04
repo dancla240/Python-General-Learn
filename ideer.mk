b0a767c2359decb2d8e9716b6125f724e17052a9
Daniel Claesson <dacl@rocketmail.com>
2025-01-04 14:08:20 +0100

    First commit

---
+++ ideer.mk
@@ -0,0 +1,26 @@
+bias variance tradeoff
+accuracy precision recall
+generell ML metodik
+overfitting underfitting
+cross validation
+hyperparameter tuning
+grid search
+feature selection
+feature engineering
+data preprocessing
+data cleaning
+data augmentation
+data normalization
+data standardization
+data scaling
+ensamble methods vs single models
+classification vs regression
+supervised vs unsupervised
+clustering
+dimensionality reduction
+model evaluation
+model selection
+model comparison
+principal component analysis
+neural networks
+curse of dimensionality (https://www.geeksforgeeks.org/k-nearest-neighbors-and-curse-of-dimensionality/?ref=header_outind)
